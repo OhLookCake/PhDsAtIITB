01b213ee72d584fa4ddf9de025320676040b487a
Eeshan <eeshan@gmail.com>
2015-10-03 16:32:57 +0530

    more visualizations added

--- scripts/plotter.R
+++ scripts/plotter.R
@@ -1,5 +1,7 @@
 
+library(plyr)
 library(ggplot2)
+library(RColorBrewer)
 
 df <- read.csv('../data/formatteddata_clean.csv', header=T)
 dfPrograms <- read.csv('../data/programsdata.csv',  header=T)
@@ -8,28 +10,48 @@ dfPrograms <- read.csv('../data/programsdata.csv',  header=T)
 
 #### C. Insights & Visualization ####
 
-p <- ggplot(data=df, aes(
-  x = Length,
-  y = reorder(Department, Length),
-  color = Department,
-  group = Department)) 
-
-
-p + geom_point(size=4,alpha=0.4, position = position_jitter(height = 0.3)) +
-  geom_errorbarh(stat = "vline", xintercept = "mean", height=0.6, size=1,
-                 aes(xmax=..x..,xmin=..x..),color="black") +
-  theme(legend.position="none") +
-  xlab("Ph.D Length") + ylab("Department")
-
-
 ggplot(data = dfPrograms, aes(
-  x = Startedin,
-  y = reorder(Department, Startedin),
-  color = Department,
-  group = Department)) + geom_point() + ylab("Department")
+                            x = Startedin,
+                            y = reorder(Department, Startedin),
+                            color = Department,
+                            group = Department)) + geom_point() + ylab("Department")
 
+coeffs <- coef(lm(FinishYear ~ StartYear, data = df))
 ggplot(data = df, aes(
-  x = StartYear,
-  y = FinishYear)) + geom_point(size=4,alpha=0.03)
+                      x = StartYear,
+                      y = FinishYear)) + geom_point(size=6,alpha=0.04, colour="#112288") +
+  coord_fixed() + geom_abline(intercept = coeffs[1], slope = coeffs[2], colour = "#EE2211", size = 2)
 
 
+p <- ggplot(data=df, aes(
+                      x = Length,
+                      y = reorder(Dept, Length),
+                      color = Dept,
+                      group = Dept)) 
+
+
+p2 <- p + geom_point(size=4,alpha=0.3, position = position_jitter(height = 0.3)) +
+          geom_errorbarh(stat = "vline", xintercept = "mean", height=0.6, size=1,
+                         aes(xmax=..x..,xmin=..x..),color="black") +
+          theme(legend.position="none") +
+          xlab("Ph.D Length") + ylab("Department")
+
+p2 + theme_bw()
+p2 + scale_colour_hue(h=c(20, 270)) + theme_bw()
+
+
+
+ggplot(data = df, aes(x = Length))  + geom_histogram(binwidth = 1, aes(y = ..density..))
+ggplot(data = df, aes(x = Length, fill=Dept))  + geom_bar(binwidth = 1, position="fill")
+ggplot(data = df, aes(x = Length, colour = Dept))  + geom_freqpoly(binwidth=1)
+
+stat_sum_single <- function(fun, geom="point", ...) {
+  stat_summary(fun.y=fun, colour="red", geom=geom, size = 3, ...)
+}
+
+ggplot(data = df, aes(x = StartYear,
+                      y = Length)) + geom_point(size = 4, alpha = 0.4) + stat_sum_single(mean, geom="line") + 
+  theme_bw() + theme(panel.grid.major =  element_blank(), panel.grid.minor = element_blank())
+
+coef(lm(Length ~ StartYear, data = df))
+cor(df$Length, df$StartYear)
