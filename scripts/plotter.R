322220d398bb7dc819e75d16d3dd914ac5f96adc
Eeshan <eeshan@gmail.com>
2015-10-20 16:05:56 +0530

    more aesthetic changes

--- scripts/plotter.R
+++ scripts/plotter.R
@@ -2,6 +2,7 @@
 library(plyr)
 library(ggplot2)
 library(RColorBrewer)
+library(gtable)
 library(scales)
 
 df <- read.csv('../data/formatteddata_clean.csv', header=T)
@@ -30,6 +31,7 @@ ggplot(data = df, aes(
   coord_fixed() + geom_abline(intercept = coeffs[1], slope = coeffs[2], colour = "#EE2211", size = 2)
 
 #By Department
+
 p <- ggplot(data=df, aes(
                       x = Length,
                       y = reorder(Dept, Length, FUN = trimmedMean),
@@ -38,17 +40,30 @@ p <- ggplot(data=df, aes(
 
 p2 <- p + geom_point(size=4,alpha=0.3, position = position_jitter(height = 0.3)) +
           geom_errorbarh(stat = "vline", xintercept = "trimmedMean", height=0.6, size=1,
-                         aes(xmax=..x..,xmin=..x..),color="black") +
-          theme(legend.position="none") +
-          xlab("Ph.D Length") + ylab("Department")
-p2 + theme_bw() + theme(legend.position="none")
-p2 + theme(legend.position="none")
+                         aes(xmax=..x..,xmin=..x..), color="black")
+
+p3 <- p2 +
+        xlab("Ph.D Length") + ylab("Department") + 
+        scale_x_continuous(breaks=1:15, limits = c(1,15))
+        
+        
+p3 +  theme(axis.text.y = element_text(size = 16, face="bold"), axis.text.x = element_text(size = 14),
+            axis.title = element_text(size=14,face="bold"), axis.title.x = element_text(vjust= -0.5)) +
+      theme(legend.position="none") +
+      theme(panel.grid.minor = element_blank())
 
 
 #Global Hist
-ggplot(data = df, aes(x = Length))  + geom_histogram(binwidth = 1, aes(y = ..density..)) +
-  scale_x_continuous(breaks=1:15, limits = c(1,15)) + scale_y_continuous(labels = percent) + 
-  theme( panel.grid.minor = element_blank(),axis.text.x =  element_text(hjust=-2.5))
+
+ggplot(data = df, aes(x = factor(Length), fill = 1))  + geom_histogram(aes(y=..count../sum(..count..))) +
+  scale_y_continuous(labels = percent_format(), limits = c(0, .35), expand = c(.01,0)) + 
+  xlab("Ph.D Length in years") + ylab("Percent of Students") +
+  theme(panel.grid.minor = element_blank(), panel.grid.major.x = element_blank(), 
+        legend.position="none") + 
+  theme(axis.text.y = element_text(size = 14, face="bold"), axis.text.x = element_text(size = 14),
+        axis.title = element_text(size=14,face="bold"),
+        axis.title.x = element_text(vjust= -0.5), axis.title.y = element_text(vjust= 1.5)) 
+
 
 ggplot(data = df, aes(x = Length, fill=Dept))  + geom_bar(binwidth = 1, position="fill")
 ggplot(data = df, aes(x = Length, colour = Dept))  + geom_freqpoly(binwidth=1)
